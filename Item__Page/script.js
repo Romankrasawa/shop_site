ad7c9aac90312adbd1aceccf1cf90dc6e6d6c96b
DeemKeL <104728821+DeemKeL@users.noreply.github.com>
2023-03-03 21:15:28 +0200

    Update script.js

--- Item__Page/script.js
+++ Item__Page/script.js
@@ -9,3 +9,22 @@ catalog__btn.addEventListener('click', function() {
 catalog__crossBtn.addEventListener('click', function() {
     catalog.classList.remove('catalog__list-toggle-class');
 })
+
+
+
+const feedback__openBtn = document.querySelector('.user-feedback__btn');
+
+feedback__openBtn.addEventListener('click', function() {
+    const feedback__div = document.querySelector('.feedback-box');
+
+    feedback__div.classList.remove('feedback-box-display');
+})
+
+
+const feedback__closeBtn = document.querySelector('.feedback__close-btn');
+
+feedback__closeBtn.addEventListener('click', function() {
+    const feedback__div = document.querySelector('.feedback-box');
+
+    feedback__div.classList.add('feedback-box-display');
+})
